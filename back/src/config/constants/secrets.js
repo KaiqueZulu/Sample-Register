0a6b13c10c68becc4cbbe56bf18aca01f6a21df0
Kaique Pires <kaiqueroberto.pires@gmail.com>
2022-07-26 19:44:39 -0300

    fixing the way to import environment variables

--- back/src/config/constants/secrets.js
+++ back/src/config/constants/secrets.js
@@ -1,1 +1,4 @@
-export const API_SECRET = process.env.API_SECRET ? process.env.API_SECRET : 'YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=';
+import { env } from 'process';
+export const API_SECRET = env.API_SECRET
+  ? env.API_SECRET
+  : 'YXV0aC1hcGktc2VjcmV0LWRldi0xMjM0NTY=';
--- back/src/index.js
+++ back/src/index.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import swaggerUi from 'swagger-ui-express';
+import { env } from 'process';
 import cors from 'cors';
-import {
-  readFile
-} from 'fs/promises';
+import { readFile } from 'fs/promises';
 
 import * as db from './config/db/initialData.js';
 
 import Routes from './routers/router.js';
 import SampleTask from './modules/sample/service/SampleTask.js';
-const swaggerDocs = JSON.parse(await readFile('./src/config/swagger/swagger.json', 'utf8'));
+const swaggerDocs = JSON.parse(
+  await readFile('./src/config/swagger/swagger.json', 'utf8')
+);
 
 const app = express();
 SampleTask.start();
@@ -23,7 +24,6 @@ app.get('/api/status', (req, res) => {
   });
 });
 
-
 app.use(express.static('./'));
 app.use(express.json());
 app.use(cors());
@@ -31,7 +31,6 @@ app.use(cors());
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/v1', Routes);
 
-
-app.listen(process.env.API_PORT, () => {
-  console.log(`Server is running on port ${process.env.API_PORT}`);
+app.listen(env.API_PORT, () => {
+  console.log(`Server is running on port ${env.API_PORT}`);
 });
